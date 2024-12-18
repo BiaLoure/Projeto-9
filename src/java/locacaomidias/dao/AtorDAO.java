815c0c5226bd440c028478ee0e217e849d9d5181
Bianca Emily <biancaemilylourenco@gmail.com>
2024-12-18 18:08:10 -0300

    Fix: Atorrr

--- src/java/locacaomidias/dao/AtorDAO.java
+++ src/java/locacaomidias/dao/AtorDAO.java
@@ -19,16 +19,19 @@ public class AtorDAO extends DAO<Ator> {
     @Override
     public void salvar(Ator obj) throws SQLException {
         PreparedStatement stmt = getConnection().prepareStatement(
-            "INSERT INTO " +
-            "ator(nome,sobrenome,data_estreia) " +
-            "VALUES( ?,?,? );" );
-
-        stmt.setString( 1, obj.getNome() );
-        stmt.setString( 2, obj.getSobrenome() );
-        stmt.setDate( 3, obj.getDataEstreia() );
-
+                """
+                INSERT INTO
+                ator( nome, sobrenome, data_estreia )
+                VALUES ( ?, ?, ? );
+                """,
+                new String[]{ "insert_id" });
+        
+        stmt.setString(1, obj.getNome());
+        stmt.setString(2, obj.getSobrenome());
+        stmt.setDate(3, obj.getDataEstreia());
+        
         stmt.executeUpdate();
-         obj.setId( Utils.getChavePrimariaAposInsercao( stmt, "insert_id" ) );
+        obj.setId(Utils.getChavePrimariaAposInsercao( stmt, "insert_id" ) );
         stmt.close();
 
     }
@@ -36,31 +39,37 @@ public class AtorDAO extends DAO<Ator> {
     @Override
     public void atualizar(Ator obj) throws SQLException {
         PreparedStatement stmt = getConnection().prepareStatement(
-            "UPDATE ator " +
-            "SET nome = ?," +   
-            "sobrenome = ?," +   
-            "data_estreia = ? " +  
-            "WHERE" +
-            " id = ?;" );
-
-        stmt.setString( 1, obj.getNome() );
-        stmt.setString( 2, obj.getSobrenome() );
-        stmt.setDate( 3, obj.getDataEstreia() );
-        stmt.setLong( 4, obj.getId() );
-
+                """
+                UPDATE ator
+                SET
+                nome = ?,
+                sobrenome = ?,
+                data_estreia = ?
+                WHERE
+                id = ?;
+                """);
+        
+        stmt.setString(1, obj.getNome());
+        stmt.setString(2, obj.getSobrenome());
+        stmt.setDate(3, obj.getDataEstreia());
+        stmt.setLong(4, obj.getId());
+     
         stmt.executeUpdate();
         stmt.close();
-
+        
     }
 
     @Override
     public void excluir(Ator obj) throws SQLException {
-        PreparedStatement stmt = getConnection().prepareStatement(
-            "DELETE FROM ator " +
-            "WHERE" +
-            " id = ?;" );
-
-        stmt.setLong( 1, obj.getId() );
+        
+         PreparedStatement stmt = getConnection().prepareStatement(
+                """
+                DELETE FROM ator
+                WHERE id = ?;
+                """);
+        
+        stmt.setLong(1, obj.getId());
+        
         stmt.executeUpdate();
         stmt.close();
     }
@@ -68,57 +77,62 @@ public class AtorDAO extends DAO<Ator> {
     @Override
     public List<Ator> listarTodos() throws SQLException {
         List<Ator> lista = new ArrayList<>();
-
-    PreparedStatement stmt = getConnection().prepareStatement(
-        "SELECT * FROM ator " +
-        "ORDER BY id;" );
-
-    ResultSet rs = stmt.executeQuery();
-    while ( rs.next() ) {
-
-        Ator a= new Ator();
-
-        a.setId( rs.getLong( "id" ) );
-        a.setNome( rs.getString( "nome" ) );
-        a.setSobrenome( rs.getString( "sobrenome" ) );
-        a.setDataEstreia( rs.getDate( "data_estreia" ) );
-       
-
-        lista.add( a );
-
-    }
-
-    rs.close();
-    stmt.close();
-
-    return lista;
+        
+        PreparedStatement stmt = getConnection().prepareStatement(
+                """
+                SELECT * FROM ator
+                ORDER BY nome, sobrenome, data_estreia;
+                """);
+        
+        ResultSet rs = stmt.executeQuery();
+        
+        while ( rs.next() ) {
+            
+            Ator a = new Ator();
+          
+            a.setId(rs.getLong("id"));
+            a.setNome(rs.getString("nome"));
+            a.setSobrenome(rs.getString("sobrenome"));
+            a.setDataEstreia(rs.getDate("data_estreia"));           
+            
+            lista.add( a );
+            
+        }
+        
+        rs.close();
+        stmt.close();
+        
+        return lista;
     }
 
     @Override
     public Ator obterPorId(Long id) throws SQLException {
+        
         Ator ator = null;
-
+        
         PreparedStatement stmt = getConnection().prepareStatement(
-            "SELECT * FROM ator " +
-            "WHERE id = ?;" );
-        stmt.setLong( 1, id );
-
+                """
+                SELECT * FROM ator
+                WHERE id = ?;
+                """);
+        
+        stmt.setLong(1, id);
+        
         ResultSet rs = stmt.executeQuery();
-
-        if ( rs.next() ) {
-
+        
+        if ( rs.next() ){
+            
             ator = new Ator();
-
-            ator.setId( rs.getLong( "id" ) );
-            ator.setNome( rs.getString( "nome" ) );
-            ator.setSobrenome( rs.getString( "sobrenome" ) );
-            ator.setDataEstreia( rs.getDate( "data_estreia" ) );
-           
+            
+            ator.setId(rs.getLong("id"));
+            ator.setNome(rs.getString("nome"));
+            ator.setSobrenome(rs.getString("sobrenome"));
+            ator.setDataEstreia(rs.getDate("data_estreia"));
         }
-
+        
         rs.close();
         stmt.close();
-
+        
         return ator;
     }
 }
